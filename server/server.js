59b92a68fbfdff7c5eabccd12062c15ad27464a7
Rob McDiarmid <robianmcd@gmail.com>
2014-07-04 13:57:56 +0000

    replaced process.cwd() with __dirname so that it will work on heroku

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var app = express();
 
 
 //Middleware
-app.use(express.static(process.cwd() + '/../client'));
-console.log(process.cwd());
+app.use(express.static(__dirname + '/../client'));
+console.log(__dirname);
 
 //Endpoints
 app.get('/api', function(req, res) {
